ea0a8792dfea7aafbec31673fbc1f2f7136ab93b
Samantha brake <sammy.brake@gmail.com>
2018-09-14 02:19:28 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -7,6 +7,11 @@ class Shoe
   def initialize(brand)
     @brand = brand
   end
+  
+  def brand=(brand)
+    @brand = brand 
+    BRANDS << brand 
+  end 
 
   def cobble
     self.condition = "new"
